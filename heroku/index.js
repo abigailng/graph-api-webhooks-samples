f226e5edb82e208ee995c029e9c6b6e3ced74084
abigailng <65820129+abigailng@users.noreply.github.com>
2021-04-14 10:20:58 +0700

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -25,7 +25,7 @@ app.get('/', function(req, res) {
   res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
 
-app.get(['/facebook', '/instagram'], function(req, res) {
+app.get(['/webhook_fb'], function(req, res) {
   if (
     req.query['hub.mode'] == 'subscribe' &&
     req.query['hub.verify_token'] == token
@@ -36,7 +36,7 @@ app.get(['/facebook', '/instagram'], function(req, res) {
   }
 });
 
-app.post('/facebook', function(req, res) {
+app.post('/webhook_fb', function(req, res) {
   console.log('Facebook request body:', req.body);
 
   if (!req.isXHubValid()) {
