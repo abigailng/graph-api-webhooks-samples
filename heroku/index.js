aeeb61079a77c31f14248f97ea14fcccbd2690c9
abigailng <65820129+abigailng@users.noreply.github.com>
2021-05-04 11:32:30 +0700

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -85,12 +85,7 @@ function callSendAPI(sender_psid, response) {
     "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
     "method": "POST",
     "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
-    }
+  }
   }); 
 }
 
